fd7edccfe9dc845ef5a9920dcc9686ab50e51b7c
Mariano Guerra <mariano@marianoguerra.org>
2016-05-15 16:19:20 +0100

    add stop, improve ui

--- apps/sbs/priv/assets/js/sonicpi.js
+++ apps/sbs/priv/assets/js/sonicpi.js
@@ -1,39 +1,74 @@
 /*globals document, window, Blockly, qwest, console*/
 (function () {
     'use strict';
-    var workspace;
+    var workspace,
+        guiId = "gui-id";
 
     function byId(id) {
         return document.getElementById(id);
     }
 
+    function getInputText(id) {
+        return byId(id).value;
+    }
+
+    function getHost() {
+        return getInputText("conf-host");
+    }
+
+    function getPort() {
+        return parseInt(getInputText("conf-port"), 10);
+    }
+
+    function sendCommand(command) {
+        return qwest.post('/cmd', command , {dataType: 'json'})
+            .then(function(xhr, response) {
+                console.log(response);
+            })
+            .catch(function(e, xhr, response) {
+                console.error(e, response);
+            }); 
+    }
+
     function runCode() {
         var code = Blockly.JavaScript.workspaceToCode(workspace),
-            guiId = "gui-id",
-            host = "localhost",
-            port = 4557;
+            host = getHost(),
+            port = getPort();
 
-        qwest.post('/cmd',
-                   {type: "/run-code", code: code, host: host, port: port, id: guiId},
-                   {dataType: 'json'})
-        .then(function(xhr, response) {
-            console.log(response);
-        })
-        .catch(function(e, xhr, response) {
-            console.error(e, response);
-        }); 
+        if (port !== port) {
+            window.alert("Invalid port value");
+            return;
+        }
+
+        return sendCommand({type: "/run-code", code: code,
+                           host: host, port: port, id: guiId});
+    }
+
+    function stopAll() {
+        var host = getHost(),
+            port = getPort();
+
+        if (port !== port) {
+            window.alert("Invalid port value");
+            return;
+        }
+
+        return sendCommand({type: "/stop-all-jobs",
+                           host: host, port: port, id: guiId});
     }
 
     function init() {
         var wsId = 'blocklyDiv',
             cont = byId(wsId),
             runBtn = byId('run-code'),
+            stopBtn = byId('stop-all'),
             toolbox = byId('toolbox');
 
         workspace = Blockly.inject(wsId, {media: './media/', toolbox: toolbox});
         cont.style.height = (window.innerHeight - 100) + "px";
 
         runBtn.addEventListener('click', runCode);
+        stopBtn.addEventListener('click', stopAll);
     }
 
     init();
